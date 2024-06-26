f474e32aa7d0d4455e4eff9c4d21dc361dc73251
HITlittlefox <963614756@qq.com>
2024-06-27 00:55:54 +0800

    28KMP

--- leetcode/editor/cn/ID28FindTheIndexOfTheFirstOccurrenceInAString .java
+++ leetcode/editor/cn/ID28FindTheIndexOfTheFirstOccurrenceInAString .java
@@ -16,27 +16,46 @@ class ID28FindTheIndexOfTheFirstOccurrenceInAString {
     class Solution {
         public int strStr(String haystack, String needle) {
 
-            int i = 0;
-            int j = 0;
+            if (needle.isEmpty()) {
+                return 0;
+            }
+            int[] next = new int[needle.length()];
+            getNextNums(next, needle);
 
-            while (i < haystack.length()) {
-                int i2 = i;
-                int j2 = 0;
-                while (haystack.charAt(i2) == needle.charAt(j2)) {
-                    i2++;
-                    j2++;
-                    if (i2 == haystack.length() && j2 != needle.length()) {
-                        return -1;
-                    }
-                    if (j2 == needle.length()) {
-                        return i;
-                    }
+            int j = -1;
+
+            for (int i = 0; i < haystack.length(); i++) {
+                while (j > 0 && haystack.charAt(i) != needle.charAt(j + 1)) {
+                    j = next[j];
                 }
-                i++;
+                if (haystack.charAt(i) == needle.charAt(j + 1)) {
+                    j++;
+                }
+                if (j == needle.length() - 1) {
+                    return (i - needle.length() + 1);
+
+                }
             }
             return -1;
 
         }
+
+        public void getNextNums(int[] next, String s) {
+            int j = -1;
+            next[0] = j;
+            for (int i = 1; i < s.length(); i++) {
+                while (j >= 0 && s.charAt(i) != s.charAt(j + 1)) {
+                    j = next[j];
+                }
+
+                if (s.charAt(i) == s.charAt(j + 1)) {
+                    j++;
+                }
+                next[i] = j;
+
+            }
+        }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
